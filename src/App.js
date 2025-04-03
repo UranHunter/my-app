dcf3124f39bd49b992775cb033313d1e17cfdb80
UranHunter <uran_hunter@mail.ru>
2025-04-04 00:00:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Transformation from './pages/Transformation';
 
 function App() {
   return (
-    <div>
-      <h1>Привет из Волшебной таблетки AI!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/transformation" element={<Transformation />} />
+      </Routes>
+    </Router>
   );
 }
 
