70e0c21655d8fd7c5be6455d620e2095ba74a782
UranHunter <uran_hunter@mail.ru>
2025-04-04 00:04:17 +0300

    Update Transformation.js

--- src/pages/Transformation.js
+++ src/pages/Transformation.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function Transformation() {
-  return <h1>🔥 Добро пожаловать в раздел "Моя трансформация"</h1>;
+  return <h1>Моя трансформация</h1>;
 }
 
 export default Transformation;
